70ce95c56330008a9e340fc5294e27163ed63da6
Aiyon860 <yonyondap23@gmail.com>
2024-02-13 19:47:20 +0700

    adding shell file

---
+++ func.sh
@@ -0,0 +1,1 @@
+function_positional_argument.sh
---
+++ function_positional_argument.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/zsh
+
+showIdentity() {
+    local name=$1
+    local age=$2
+    echo "$name: $age"
+}
+
+showIdentity "Daniel" 18
+
+echo 
